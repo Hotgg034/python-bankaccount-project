a4eb63e08bbf19a18568e46d666d67bb8201b26c
Hotgg034 <ghaithabbasi07@gmail.com>
2026-05-10 16:32:07 +0200

    Added a new withdraw function

--- bank.py
+++ bank.py
@@ -1,2 +1,7 @@
 def deposit(balance, amount):
 	return balance + amount
+
+def withdraw(balance, amount):
+    if amount > balance:
+        return "Error: Insufficient funds"
+    return balance - amount
